19dcb9c7d2bd54e5f637a1cbbfa130726fee9b4b
Vincent Rémond <vincent.remond@gmail.com>
2021-05-19 15:51:41 +0200

    Use argument for notes

--- commands/notes.py
+++ commands/notes.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 
 import os.path
@@ -9,10 +10,18 @@ def write_all_text(path, contents):
     file1.write(contents)
     file1.close()
 
+def getName(args):
+    if(len(args) > 1):
+        return args[1]
+    else:
+        return "Notes"
+
+name = getName(sys.argv)
+
 currentDir = os.getcwd()
 filename = 'notes.md'
 if not path.exists(filename):
-    write_all_text(filename, "# Notes\n\n\n")
+    write_all_text(filename, f'# {name}\n\n\n')
 
 subprocess.Popen(
     ['C:\Program Files\Microsoft VS Code\Code.exe', currentDir, "--goto", f'{filename}:3'], cwd=currentDir)
