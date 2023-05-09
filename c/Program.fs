fa7ac66c4ba04576fc4808c5e107c97a773b67cb
Vincent Rémond <vincent.remond@gmail.com>
2023-05-09 18:54:42 +0200

    V3

---
+++ c/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open Common
+
+[<EntryPoint>]
+[<STAThread>]
+let main _ =
+    let vsCodePath = @"C:\Program Files\Microsoft VS Code\Code.exe"
+    let currentDirectory = Environment.CurrentDirectory
+
+    Process.start (StartDirectory currentDirectory) (Executable vsCodePath) [| currentDirectory |]
+
+    0 // return an integer exit code
