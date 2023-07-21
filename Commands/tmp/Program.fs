b078163150b83936ee1ea0001040d1e5efdb2f5e
Vincent Rémond <vincent.remond@gmail.com>
2023-07-21 18:39:50 +0200

    fix: tmp

--- Commands/tmp/Program.fs
+++ Commands/tmp/Program.fs
@@ -4,48 +4,43 @@ open Common
 open FSharpPlus
 open ExplorerShortcuts.Common
 
-[<EntryPoint>]
-[<STAThread>]
-let main _ =
-    let displayPrompt () =
-        Console.Title <- " >>< TMP ><<"
-        Console.BackgroundColor <- ConsoleColor.DarkBlue
-        Console.ForegroundColor <- ConsoleColor.White
-        Console.Clear()
+let displayPrompt () =
+    Console.Title <- " >>< TMP ><<"
+    Console.BackgroundColor <- ConsoleColor.DarkBlue
+    Console.ForegroundColor <- ConsoleColor.White
+    Console.Clear()
 
-        [
-            ""
-            ""
-            "                ████████╗███╗   ███╗██████╗ "
-            "                ╚══██╔══╝████╗ ████║██╔══██╗"
-            "                   ██║   ██╔████╔██║██████╔╝"
-            "                   ██║   ██║╚██╔╝██║██╔═══╝ "
-            "                   ██║   ██║ ╚═╝ ██║██║     "
-            "                   ╚═╝   ╚═╝     ╚═╝╚═╝     "
-            ""
-            ""
+    [
+        ""
+        ""
+        "                ████████╗███╗   ███╗██████╗ "
+        "                ╚══██╔══╝████╗ ████║██╔══██╗"
+        "                   ██║   ██╔████╔██║██████╔╝"
+        "                   ██║   ██║╚██╔╝██║██╔═══╝ "
+        "                   ██║   ██║ ╚═╝ ██║██║     "
+        "                   ╚═╝   ╚═╝     ╚═╝╚═╝     "
+        ""
+        ""
 
-        ]
-        |> List.iter (printfn "%s")
-
-        printf "                <name>: "
-
-
-    let fixedName n =
-        n |> Regex.replace @"[^\w\d]" "-" |> Regex.replace "-+" "-"
-
-    displayPrompt ()
-    let name = Console.ReadLine()
-    let today = DateTime.Now.ToString("yyyy-MM-dd")
-    let folderName = $"{today}--{fixedName name}"
-    let folder = @"D:\TMP\" </> folderName
-    Directory.CreateDirectory(folder) |> ignore
-    let notes = folder </> "notes.md"
-    [ $"# {name}"; ""; $"_{today}_"; ""; ""; "" ] |> (File.writeAllLines notes)
-
-    Process.startAndForget (StartDirectory folder) (Executable "cmd.exe") [
-        "/c"
-        $"code.cmd \"{folder}\" --goto notes.md:5:0"
     ]
+    |> List.iter (printfn "%s")
 
-    0
+    printf "                <name>: "
+
+
+let fixedName n =
+    n |> Regex.replace @"[^\w\d]" "-" |> Regex.replace "-+" "-"
+
+displayPrompt ()
+let name = Console.ReadLine()
+let today = DateTime.Now.ToString("yyyy-MM-dd")
+let folderName = $"{today}--{fixedName name}"
+let folder = @"D:\TMP\" </> folderName
+Directory.CreateDirectory(folder) |> ignore
+let notes = folder </> "notes.md"
+[ $"# {name}"; ""; $"_{today}_"; ""; ""; "" ] |> (File.writeAllLines notes)
+
+Process.startAndForget (StartDirectory folder) (Executable "cmd.exe") [
+    "/c"
+    $"code.cmd \"{folder}\" --goto notes.md:5:0"
+]
