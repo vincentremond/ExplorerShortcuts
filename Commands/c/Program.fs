7f0997c441ac09b1765d3fcfcb4a3d663635f3dc
Vincent Rémond <vincent.remond@gmail.com>
2023-12-04 18:42:51 +0100

    Modify path retrieval for Visual Studio Code
    The code now checks multiple potential paths, including the Local Application Data folder, to find the location of the Visual Studio Code executable. This update responds to certain install circumstances where the executable may not be in the assumed path.

--- Commands/c/Program.fs
+++ Commands/c/Program.fs
@@ -1,13 +1,28 @@
 ﻿open System
+open System.IO
 open ExplorerShortcuts.Common
 open FSharpPlus
 
 [<EntryPoint>]
 [<STAThread>]
 let main _ =
-    let vsCodePath = @"C:\Program Files\Microsoft VS Code\Code.exe"
 
-    let (StartDirectory strStartDirectory) as startDirectory =
+    let possiblePaths =
+        [
+            @"C:\Program Files"
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) </> @"Programs"
+        ]
+        |> List.map (fun x ->
+            x
+            </> @"Microsoft VS Code\Code.exe"
+        )
+
+    let vsCodePath =
+        possiblePaths
+        |> List.tryFind File.Exists
+        |> Option.defaultWith (fun _ -> failwith $"Visual Studio Code not found (%A{possiblePaths})")
+
+    let StartDirectory strStartDirectory as startDirectory =
         StartDirectory.CurrentDirectory
 
     let arg =
