ef3135f51d489f1d3cd369d7281316a25980eb4e
Vincent Rémond <vincent.remond@gmail.com>
2025-05-19 10:44:14 +0200

    feat: Enhance TMP directory management and integrate Pinicola.FSharp project

--- Commands/tmp/Program.fs
+++ Commands/tmp/Program.fs
@@ -5,6 +5,7 @@ open System.Diagnostics
 open System.IO
 open ExplorerShortcuts.Common
 open Newtonsoft.Json
+open Pinicola.FSharp
 open Pinicola.FSharp.SpectreConsole
 open Spectre.Console
 
@@ -13,10 +14,16 @@ type CreateDotnetProject =
     | No
     | Yes of Type: string
 
-let preferredLocations =
-    let userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+type Location =
+    | Default
+    | Personal
 
-    seq {
+let userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+let preferredPersonalLocations = list { yield "Perso" </> "tmp" }
+
+let preferredDefaultLocations =
+
+    list {
         yield "G:\My Drive\TMP"
         yield (userProfile </> "TMP")
         yield @"D:\TMP\"
@@ -26,15 +33,8 @@ let preferredLocations =
              |> Directory.getDirectories "OneDrive*"
              |> Seq.map (fun onedrive -> onedrive </> "TMP"))
     }
-    |> List.ofSeq
 
-let tmpLocation =
-    preferredLocations
-    |> List.tryPick Directory.tryPath
-    |> Option.defaultWith (fun _ -> failwith $"Could not find preferred location %A{preferredLocations}")
-    |> _.FullName
-
-let displayPrompt () =
+let displayPrompt location =
     Console.Title <- " ... TMP ... "
 
     AnsiConsole.Background <- Color.DarkBlue
@@ -42,10 +42,8 @@ let displayPrompt () =
     AnsiConsole.Clear()
     AnsiConsole.Write(FigletText(SpectreConsole.FigletFont.AnsiShadow, "TMP"))
 
+    AnsiConsole.markupLineInterpolated $"Working directory: [bold]{location}[/]"
     let project = AnsiConsole.Ask<string>("What are you working on today?")
-
-    // let createDotnetProject =
-    //     AnsiConsole.Confirm("Create a new .NET project?", defaultValue = false)
 
     let selectionPrompt = SelectionPrompt()
     selectionPrompt.Title <- "Create a new .NET project?"
@@ -63,89 +61,107 @@ let displayPrompt () =
 
     (project, createDotnetProject)
 
-let name, createDotnetProject = displayPrompt ()
-let thisMonth = DateTime.Now.ToString("yyyy-MM")
-let today = DateTime.Now.ToString("yyyy-MM-dd")
-let fixedName = name |> Regex.replace @"[^\w\d]" "-" |> Regex.replace "-+" "-"
-let newFolderName = $"{today}--{fixedName}"
+[<EntryPoint>]
+let main args =
+    let location =
+        match args with
+        | [| x |] when String.equalsInvariantCultureIgnoreCase x "personal" -> Personal
+        | _ -> Default
 
-// Create month folder if it doesn't exist
-let monthFolder = tmpLocation </> thisMonth
+    let locationSource =
+        match location with
+        | Default -> preferredDefaultLocations
+        | Personal -> preferredPersonalLocations
 
-if not (Directory.Exists monthFolder) then
-    Directory.CreateDirectory(monthFolder) |> ignore
+    let tmpLocation =
+        preferredDefaultLocations
+        |> List.tryPick Directory.tryPath
+        |> Option.defaultWith (fun _ -> failwith $"Could not find preferred location %A{locationSource}")
+        |> _.FullName
 
-let newTmpFolder = tmpLocation </> newFolderName
-Directory.CreateDirectory(newTmpFolder) |> ignore
+    let name, createDotnetProject = displayPrompt location
+    let thisMonth = DateTime.Now.ToString("yyyy-MM")
+    let today = DateTime.Now.ToString("yyyy-MM-dd")
+    let fixedName = name |> Regex.replace @"[^\w\d]" "-" |> Regex.replace "-+" "-"
+    let newFolderName = $"{today}--{fixedName}"
 
-let notesFileName = "notes.md"
-let notesFilePath = newTmpFolder </> notesFileName
+    // Create month folder if it doesn't exist
+    let monthFolder = tmpLocation </> thisMonth
 
-[
-    $"# {name}"
-    ""
-    $"Date: _{today}_"
-    ""
-    "**your notes here**"
-]
-|> (File.writeAllLines notesFilePath)
+    if not (Directory.Exists monthFolder) then
+        Directory.CreateDirectory(monthFolder) |> ignore
 
-let workspaceContents =
-    {|
-        folders = [ {| path = "." |} ]
-        settings = {| ``window.title`` = name |}
-    |}
-    |> (fun x -> JsonConvert.SerializeObject(x, Formatting.Indented))
+    let newTmpFolder = tmpLocation </> newFolderName
+    Directory.CreateDirectory(newTmpFolder) |> ignore
 
-let workspaceFile = newTmpFolder </> $"_{fixedName}_.code-workspace"
+    let notesFileName = "notes.md"
+    let notesFilePath = newTmpFolder </> notesFileName
 
-File.writeAllText workspaceFile workspaceContents
-File.hide workspaceFile
+    [
+        $"# {name}"
+        ""
+        $"Date: _{today}_"
+        ""
+        "**your notes here**"
+    ]
+    |> (File.writeAllLines notesFilePath)
 
-let startInfo =
-    ProcessStartInfo(
-        "c.exe",
-        [
-            "open"
-            "--file"
-            notesFilePath
-            "--line"
-            "5"
-            "--column"
-            "0"
-        ]
-    )
+    let workspaceContents =
+        {|
+            folders = [ {| path = "." |} ]
+            settings = {| ``window.title`` = name |}
+        |}
+        |> (fun x -> JsonConvert.SerializeObject(x, Formatting.Indented))
 
-startInfo.WorkingDirectory <- newTmpFolder
-startInfo.WindowStyle <- ProcessWindowStyle.Hidden
-startInfo.UseShellExecute <- true
-let _process = Process.Start(startInfo)
-let _exited = _process.WaitForExit(TimeSpan.FromSeconds(5.))
+    let workspaceFile = newTmpFolder </> $"_{fixedName}_.code-workspace"
 
-match createDotnetProject with
-| CreateDotnetProject.No -> ()
-| CreateDotnetProject.Yes ``type`` ->
+    File.writeAllText workspaceFile workspaceContents
+    File.hide workspaceFile
 
-    Rule() |> Rule.withTitle "Create a new .NET project" |> AnsiConsole.write
-
-    let targetFolder = newTmpFolder </> fixedName
-
-    Directory.CreateDirectory(targetFolder) |> ignore
-
-    let dotnetStartInfo =
+    let startInfo =
         ProcessStartInfo(
-            "InitProject.exe",
+            "c.exe",
             [
-                "--lang"
-                ``type``
+                "open"
+                "--file"
+                notesFilePath
+                "--line"
+                "5"
+                "--column"
+                "0"
             ]
         )
 
-    dotnetStartInfo.WorkingDirectory <- targetFolder
+    startInfo.WorkingDirectory <- newTmpFolder
+    startInfo.WindowStyle <- ProcessWindowStyle.Hidden
+    startInfo.UseShellExecute <- true
+    let _process = Process.Start(startInfo)
+    let _exited = _process.WaitForExit(TimeSpan.FromSeconds(5.))
 
-    let _dotnetProcess = Process.Start(dotnetStartInfo)
-    let _dotnetExited = _dotnetProcess.WaitForExit()
+    match createDotnetProject with
+    | CreateDotnetProject.No -> ()
+    | CreateDotnetProject.Yes ``type`` ->
 
-    AnsiConsole.Confirm("Press enter to exit ?", defaultValue = true) |> ignore
+        Rule() |> Rule.withTitle "Create a new .NET project" |> AnsiConsole.write
 
-()
+        let targetFolder = newTmpFolder </> fixedName
+
+        Directory.CreateDirectory(targetFolder) |> ignore
+
+        let dotnetStartInfo =
+            ProcessStartInfo(
+                "InitProject.exe",
+                [
+                    "--lang"
+                    ``type``
+                ]
+            )
+
+        dotnetStartInfo.WorkingDirectory <- targetFolder
+
+        let _dotnetProcess = Process.Start(dotnetStartInfo)
+        let _dotnetExited = _dotnetProcess.WaitForExit()
+
+        AnsiConsole.Confirm("Press enter to exit ?", defaultValue = true) |> ignore
+
+    ()
