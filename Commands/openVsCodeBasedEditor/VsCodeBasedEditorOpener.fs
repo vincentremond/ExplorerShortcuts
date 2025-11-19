c5d94917b45cbb8a7318302ad5cee2e82e949349
Vincent Rémond <vincent.remond@gmail.com>
2025-11-19 20:04:03 +0100

    feat: Add OpenDirectory command to allow opening a directory in the editor

--- Commands/openVsCodeBasedEditor/VsCodeBasedEditorOpener.fs
+++ Commands/openVsCodeBasedEditor/VsCodeBasedEditorOpener.fs
@@ -11,10 +11,12 @@ open Pinicola.FSharp.SpectreConsole
 [<RequireQualifiedAccess>]
 type CliOptions =
     | OpenFile
+    | OpenDirectory
     | OpenCurrentDirectory
 
 type CommandLineOptions =
-    | OpenFile of FileName: string * Line: int32 * Column: int32 option * Directory: string option
+    | OpenFile of FilePath: string * Line: int32 * Column: int32 option * Directory: string option
+    | OpenDirectory of FolderPath: string
     | OpenCurrentDirectory
 
 let commandLineParser editorName =
@@ -27,27 +29,63 @@ let commandLineParser editorName =
     fargo {
         match!
             (cmd "open" "o" $"Open a file in {editorName}" |>> CliOptions.OpenFile)
+            <|> (cmd "opendir" null $"Open a directory in {editorName}" |>> CliOptions.OpenDirectory)
             <|> (ret CliOptions.OpenCurrentDirectory)
         with
         | CliOptions.OpenFile ->
-            let! line =
+            let! file = opt "file" "f" "file" "The file to open" |> reqOpt
+            and! directory = opt "directory" "d" "directory" "The directory to open the file in"
+
+            and! line =
                 opt "line" "l" "line" "The line number to open the file at"
                 |> optParse tryParseInt
                 |> defaultValue 0
 
-            let! column =
+            and! column =
                 opt "column" "c" "column" "The column number to open the file at"
                 |> optParse tryParseInt
 
-            let! directory = opt "directory" "d" "directory" "The directory to open the file in"
-
-            let! file = opt "file" "f" "file" "The file to open" |> reqOpt
             return OpenFile(file, line, column, directory)
+        | CliOptions.OpenDirectory ->
+            let! folderPath = opt "directory" "d" "directory" "The directory to open" |> reqOpt
+            return OpenDirectory(folderPath)
         | CliOptions.OpenCurrentDirectory -> return OpenCurrentDirectory
 
     }
 
 let main shortCutName editorName possiblePaths =
+
+    let openFile editorExePath (fileName: string) directory line column =
+        let directory =
+            match directory with
+            | Some directory -> directory
+            | None -> Path.GetDirectoryName(fileName)
+
+        let gotoArgValue =
+            match column with
+            | Some column -> $"%s{fileName}:%d{line}:%d{column}"
+            | None -> sprintf $"%s{fileName}:%d{line}"
+
+        let startDirectory = StartDirectory.With directory
+
+        Process.startAndForget startDirectory (Executable editorExePath) [|
+            startDirectory.Value
+            "--goto"
+            gotoArgValue
+        |]
+
+    let openDirectory editorExePath (startDirectory: StartDirectory) =
+
+        let workspaceFile =
+            Directory.GetFiles(startDirectory.Value, "*.code-workspace")
+            |> Array.tryExactlyOne
+
+        let arg =
+            match workspaceFile with
+            | Some file -> file
+            | None -> startDirectory.Value
+
+        Process.startAndForget startDirectory (Executable editorExePath) [| arg |]
 
     FargoCmdLine.run
         shortCutName
@@ -65,45 +103,19 @@ let main shortCutName editorName possiblePaths =
                 match options with
                 | OpenFile(fileName, line, column, directory) ->
 
-                    let directory =
-                        match directory with
-                        | Some directory -> directory
-                        | None -> Path.GetDirectoryName(fileName)
+                    openFile editorExePath fileName directory line column
 
-                    let gotoArgValue =
-                        match column with
-                        | Some column -> $"%s{fileName}:%d{line}:%d{column}"
-                        | None -> sprintf $"%s{fileName}:%d{line}"
-
-                    let startDirectory = StartDirectory.With directory
-
-                    Process.startAndForget startDirectory (Executable editorExePath) [|
-                        startDirectory.Value
-                        "--goto"
-                        gotoArgValue
-                    |]
-
+                | OpenDirectory folderPath ->
+                    let startDirectory = StartDirectory.With folderPath
+                    openDirectory editorExePath startDirectory
                 | OpenCurrentDirectory ->
-
                     let startDirectory = StartDirectory.CurrentDirectory
+                    openDirectory editorExePath startDirectory
 
-                    let workspaceFile =
-                        Directory.GetFiles(startDirectory.Value, "*.code-workspace")
-                        |> Array.tryExactlyOne
-
-                    let arg =
-                        match workspaceFile with
-                        | Some file -> file
-                        | None -> startDirectory.Value
-
-                    Process.startAndForget startDirectory (Executable editorExePath) [| arg |]
             with e ->
                 MessageBox.show "error" (e.ToString())
 
-                File.WriteAllText(
-                    $"explorershortcuts.{shortCutName}.error.{DateTimeOffset.Now:yyyyMMddHHmmssfff}.txt",
-                    e.ToString()
-                )
+                File.WriteAllText($"explorershortcuts.{shortCutName}.error.{DateTimeOffset.Now:yyyyMMddHHmmssfff}.txt", e.ToString())
 
                 reraise ()
         )
